2b170fccec48d6fb0e1ed5c4cd5247a4c112460b
ahmetdlbs <ahmetdlbs04@gmail.com>
2024-11-19 12:01:56 +0300

    feat: Add Redis client export functionality 🦅

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -3,51 +3,48 @@ import { redisConfig } from '../config/database.config';
 
 let client: RedisClientType | null = null;
 
-const getClient = async (): Promise<RedisClientType | null> => {
-    if (!redisConfig.enabled) return null;
+const initializeClient = () => {
+  if (redisConfig.enabled && !client) {
+    client = createClient({
+      socket: {
+        host: redisConfig.host,
+        port: redisConfig.port
+      },
+      password: redisConfig.password
+    });
 
-    if (!client) {
-        client = createClient({
-            socket: {
-                host: redisConfig.host,
-                port: redisConfig.port
-            },
-            password: redisConfig.password
-        });
-
-        await client.connect();
-    }
-    return client;
+    client.connect().catch(err => {
+      console.error('Error connecting to Redis:', err);
+    });
+  }
 };
 
+initializeClient();
+
 export const redisService = {
-    get: async <T>(key: string): Promise<T | null> => {
-        const client = await getClient();
-        if (!client) return null;
+  get: async <T>(key: string): Promise<T | null> => {
+    if (!client) return null;
+    const data = await client.get(key);
+    return data ? JSON.parse(data) : null;
+  },
 
-        const data = await client.get(key);
-        return data ? JSON.parse(data) : null;
-    },
+  set: async <T>(key: string, value: T, ttl?: number): Promise<void> => {
+    if (!client) return;
+    await client.setEx(key, ttl ?? redisConfig.ttl, JSON.stringify(value));
+  },
 
-    set: async <T>(key: string, value: T, ttl?: number): Promise<void> => {
-        const client = await getClient();
-        if (!client) return;
-        await client.setEx(key, (ttl ? ttl : redisConfig.ttl), JSON.stringify(value));
-    },
-
-    del: async (patterns: string | string[]): Promise<void> => {
-        const client = await getClient();
-        if (!client) return;
-        const patternArray = Array.isArray(patterns) ? patterns : [patterns];
-
-        for (const pattern of patternArray) {
-            const keys = await client.keys(pattern);
-            if (keys.length) {
-                await client.del(keys);
-            }
-        }
-    },
-    client: () => {
-        return getClient();
+  del: async (patterns: string | string[]): Promise<void> => {
+    if (!client) return;
+    const patternArray = Array.isArray(patterns) ? patterns : [patterns];
+    for (const pattern of patternArray) {
+      const keys = await client.keys(pattern);
+      if (keys.length) {
+        await client.del(keys);
+      }
     }
+  },
+
+  client: (): any | null => {
+    return client;
+  }
 };
